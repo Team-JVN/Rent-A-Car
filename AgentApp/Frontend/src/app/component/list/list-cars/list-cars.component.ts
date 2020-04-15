91c3489cb82ece0718c5b978c99cff34ad6d4faa
NikolaBrodic <nikola.brodic97@gmail.com>
2020-04-15 22:34:37 +0200

    Redesign list cars page

--- AgentApp/Frontend/src/app/component/list/list-cars/list-cars.component.ts
+++ AgentApp/Frontend/src/app/component/list/list-cars/list-cars.component.ts
@@ -8,6 +8,9 @@ import { Car } from './../../../model/car';
 import { MatTableDataSource } from '@angular/material/table';
 import { Component, OnInit } from '@angular/core';
 import { EditCarComponent } from '../../edit/edit-car/edit-car.component';
+import { FuelType } from 'src/app/model/fuelType';
+import { GearBoxType } from 'src/app/model/gearboxType';
+import { BodyStyle } from 'src/app/model/bodyStyle';
 
 @Component({
   selector: 'app-list-cars',
@@ -15,7 +18,8 @@ import { EditCarComponent } from '../../edit/edit-car/edit-car.component';
   styleUrls: ['./list-cars.component.css']
 })
 export class ListCarsComponent implements OnInit {
-  displayedColumns: string[] = ['make', 'model', 'fuelType', 'gearBoxType', 'bodyStyle', 'mileageInKm', 'kidsSeats', 'availableTracking', 'buttons'];
+
+  displayedColumns: string[] = ['car'];
   carsDataSource: MatTableDataSource<Car>;
   successCreated: Subscription;
 
@@ -33,16 +37,23 @@ export class ListCarsComponent implements OnInit {
   }
 
   fetchCars() {
-    this.carService.getCars().subscribe(
-      (data: Car[]) => {
-        this.carsDataSource = new MatTableDataSource(data);
-      },
-      (httpErrorResponse: HttpErrorResponse) => {
-        const data: Car[] = []
-        this.carsDataSource = new MatTableDataSource(data)
-        this.toastr.error(httpErrorResponse.error.message, 'Show Cars');
-      }
-    );
+
+    const car1 = new Car("Opel", "Astra", new FuelType("Petrol"), new GearBoxType("6-speed Manual"), new BodyStyle("Hatchback"), 68500, 2, true);
+    const car2 = new Car("Ford", "Fiesta", new FuelType("Petrol"), new GearBoxType("5-speed Manual"), new BodyStyle("Hatchback"), 141000, 1, false);
+    const car3 = new Car("VW", "Passat", new FuelType("Diesel"), new GearBoxType("6-speed Automatic"), new BodyStyle("Limousine"), 125000, 3, true);
+    const car4 = new Car("BMW", "X5", new FuelType("Diesel"), new GearBoxType("7-speed Automatic"), new BodyStyle("SUV"), 30400, 3, true);
+    this.carsDataSource = new MatTableDataSource([car1, car2, car3, car4]);
+
+    // this.carService.getCars().subscribe(
+    //   (data: Car[]) => {
+    //     this.carsDataSource = new MatTableDataSource(data);
+    //   },
+    //   (httpErrorResponse: HttpErrorResponse) => {
+    //     const data: Car[] = []
+    //     this.carsDataSource = new MatTableDataSource(data)
+    //     this.toastr.error(httpErrorResponse.error.message, 'Show Cars');
+    //   }
+    // );
   }
 
   edit(element: Car) {
