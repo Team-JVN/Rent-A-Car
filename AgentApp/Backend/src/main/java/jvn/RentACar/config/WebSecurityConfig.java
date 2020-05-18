83897b01bf8564c730cbade716fd6f5f7466c3b3
DanijelRadakovic <danijelradakovic1996@gmail.com>
2020-05-18 13:25:08 +0200

    Generate certificates for agent application.

--- AgentApp/Backend/src/main/java/jvn/RentACar/config/WebSecurityConfig.java
+++ AgentApp/Backend/src/main/java/jvn/RentACar/config/WebSecurityConfig.java
@@ -116,4 +116,31 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                                 "/**/*.css", "/**/*.js", "/assets/**", "/*.jpg");
         }
 
+        @Bean
+        public EmbeddedServletContainerFactory servletContainer() {
+                TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory() {
+                        @Override
+                        protected void postProcessContext(Context context) {
+                                SecurityConstraint securityConstraint = new SecurityConstraint();
+                                securityConstraint.setUserConstraint("CONFIDENTIAL");
+                                SecurityCollection collection = new SecurityCollection();
+                                collection.addPattern("/*");
+                                securityConstraint.addCollection(collection);
+                                context.addConstraint(securityConstraint);
+                        }
+                };
+                
+                tomcat.addAdditionalTomcatConnectors(redirectConnector());
+                return tomcat;
+        }
+                
+        private Connector redirectConnector() {
+                Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+                connector.setScheme("http");
+                connector.setPort(8080);
+                connector.setSecure(false);
+                connector.setRedirectPort(8090);
+                
+                return connector;       
+        }
 }
