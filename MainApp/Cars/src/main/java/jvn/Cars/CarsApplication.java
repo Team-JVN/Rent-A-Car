89f5be2192c7194acc82fbf549be873de55a92e6
JelenaPopov <popov.jelena2@gmail.com>
2020-05-20 14:55:14 +0200

    Change confirguration variables for Car Service.

--- MainApp/Cars/src/main/java/jvn/Cars/CarsApplication.java
+++ MainApp/Cars/src/main/java/jvn/Cars/CarsApplication.java
@@ -2,19 +2,19 @@ package jvn.Cars;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.modelmapper.ModelMapper;
-import org.springframework.context.annotation.Bean;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class CarsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CarsApplication.class, args);
 	}
 
-	@Bean
-	public ModelMapper modelMapper() {
-		return new ModelMapper();
-	}
+	// @Bean
+	// public ModelMapper modelMapper() {
+	// return new ModelMapper();
+	// }
 
 }
