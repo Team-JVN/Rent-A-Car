05339dd65d81a4c28b68f12354de6103985e86b2
JelenaPopov <popov.jelena2@gmail.com>
2020-05-20 13:27:25 +0200

    Change configuration for zuul and eureka.

--- MainApp/ZuulAndFrontend/Zuul/src/main/java/jvn/Zuul/ZuulApplication.java
+++ MainApp/ZuulAndFrontend/Zuul/src/main/java/jvn/Zuul/ZuulApplication.java
@@ -6,11 +6,13 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableFeignClients
 @EnableZuulProxy
+@RestController
 public class ZuulApplication {
 
 	public static void main(String[] args) {
