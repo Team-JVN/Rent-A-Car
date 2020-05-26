1cb91f31f80187ffcafa2aafe8f14b7833aff47d
violetamaric <57189544+violetamaric@users.noreply.github.com>
2020-05-26 22:31:09 +0200

    Added classes in model of renting service.

---
+++ MainApp/Renting/src/main/java/jvn/Renting/model/RentRequest.java
@@ -0,0 +1,39 @@
+package jvn.Renting.model;
+
+import jvn.Renting.enumeration.RentRequestStatus;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class RentRequest {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private RentRequestStatus rentRequestStatus;
+
+    @ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    private Long client;
+
+    @Column(nullable = false)
+    private Double totalPrice;
+
+    @OneToMany(mappedBy = "rentRequest", fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    private Set<RentInfo> rentInfos = new HashSet<RentInfo>();
+
+    @OneToMany(mappedBy = "rentRequest", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private Set<Message> messages = new HashSet<>();
+
+    @ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    private Long createdBy;
+}
