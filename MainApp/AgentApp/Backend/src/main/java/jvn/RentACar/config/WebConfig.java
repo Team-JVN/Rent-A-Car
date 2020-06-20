4267e269cdb82c2c0dfa5aa09b47b2e0a01783af
JelenaPopov <popov.jelena2@gmail.com>
2020-06-20 05:57:08 +0200

    Made changes for soap.

--- MainApp/AgentApp/Backend/src/main/java/jvn/RentACar/config/WebConfig.java
+++ MainApp/AgentApp/Backend/src/main/java/jvn/RentACar/config/WebConfig.java
@@ -6,22 +6,22 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-// @Configuration
-// public class WebConfig implements WebMvcConfigurer {
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
 
-//     @Override
-//     public void addCorsMappings(CorsRegistry registry) {
-//         registry.addMapping("/**").
-//                 allowedOrigins("http://localhost:4200").allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
-//     }
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").
+                allowedOrigins("http://localhost:4200").allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+    }
 
-//     @Override
-//     public void addViewControllers(final ViewControllerRegistry registry) {
-//         registry.addViewController("/").setViewName("forward:/index.html");
-//         registry.addViewController("/index.html");
+    @Override
+    public void addViewControllers(final ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("forward:/index.html");
+        registry.addViewController("/index.html");
 
-//     }
-// }
-
-public class WebConfig {
+    }
 }
+
+//public class WebConfig {
+//}
